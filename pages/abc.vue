d21771cb7fb5cc1c76f57d1b8ec679eaa6858666
Baron <dontongso2@gmail.com>
2022-07-22 00:36:48 +0700

    test v for

--- pages/abc.vue
+++ pages/abc.vue
@@ -1,18 +1,48 @@
 <template>
-  <div>
-    <Header name="Baron"></Header>
-    <p>trang abc</p>
+  <div class="abc-wrapper">
+    <div v-for="item in users" :key="item.cmnd" class="item">
+      <p :class="item.class ? 'color-' + item.class : ''">{{ item.name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-
 export default {
-  name: "AbcPage",
+  name: "abc",
 
-  components: {
-    Header,
+  props: {
+    usersArray: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  data() {
+    return {
+      users: [
+        { name: "vin", cmnd: 1234, class: "red" },
+        { name: "ky", cmnd: 7492, class: "blue" },
+        { name: "nguyen", cmnd: 43345 },
+      ],
+    };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.color-red {
+  color: red;
+}
+.color-blue {
+  color: blue;
+}
+.abc-wrapper {
+  display: flex;
+  .item {
+    height: 200px;
+    width: 100px;
+    background-color: gray;
+    color: white;
+  }
+}
+</style>
