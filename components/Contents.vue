99f9fe25a5e6c6fef0622a15dd0e9f1ee0cb3d6a
Phan Ky <phanky@MacBook-Air-cua-Nguyen.local>
2022-07-21 23:14:39 +0700

    new

---
+++ components/Contents.vue
@@ -0,0 +1,342 @@
+<template>
+    
+    <div class="content pd70">
+      <div class="headding-events row">
+        <div class="headding-left pd15">
+          <i class="fa fa-signal" aria-hidden="true"></i>
+          <h1>top movies:</h1>
+          <h2 class="color">2019</h2>
+        </div>
+        <div class="headding-right pd15">
+          <a href=""
+            ><button class="color">
+              <i class="fa fa-arrow-left" aria-hidden="true"></i></button
+          ></a>
+          <a href=""
+            ><button class="color">
+              <i class="fa fa-arrow-right" aria-hidden="true"></i></button
+          ></a>
+        </div>
+      </div>
+      <div class="film-list5 pd15 row">
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider1.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Dareing Joker</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider2.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Furious Race</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider3.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>The Walk</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider4.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Blood Day War</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider5.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Mr.Cartoon</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+      </div>
+      <div class="trailer">
+        <div class="text-background">
+          <div class="text-trailer">
+            <p>Action, Acventure, Fantasy</p>
+            <h4>ToDay In Theater</h4>
+            <p>Researved Your Seat, Before You Miss Tomorrow</p>
+            <div class="btn-sub"><button>Discover All</button></div>
+          </div>
+          <div class="videoyt">
+            <iframe
+              width="600px"
+              height="380px"
+              src="https://www.youtube.com/embed/1Q8fG0TtVAY"
+              title="WONDER WOMAN - Official Trailer [HD]"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="headding-events2 row">
+        <div class="headding-left pd15">
+          <i class="fa fa-th-large" aria-hidden="true"></i>
+          <h1>new</h1>
+          <h2 class="color">item</h2>
+        </div>
+        <div class="headding-bot">
+          <ul>
+            <li><a href="">NEW RELEASES</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV SERIES</a></li>
+            <li><a href="">CARTOON</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="film-list10 pd15 row">
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider4.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Blood Day War</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider2.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Racing Field</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider5.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Mr.Cartoon</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider1.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Dareing Joker</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider3.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Running Shoot</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider3.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Running Shoot</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider5.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Mr.Cartoon</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider2.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Raccing Field</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider4.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Blood Day War</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+        <div class="film-hot fl5">
+          <div class="animation-film">
+            <i class="fa fa-play-circle" aria-hidden="true"></i>
+          </div>
+          <div class="film-name">
+            <img src="@/assets/image/slider1.jpeg" alt="" />
+          </div>
+          <div class="film-number">
+            <h3>Dareing Joker</h3>
+            <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i>256</a>
+            <a href=""><i class="fa fa-eye" aria-hidden="true"></i>45</a>
+            <button><i class="fa fa-star" aria-hidden="true"></i>4.5</button>
+          </div>
+        </div>
+      </div>
+      <div class="background-input">
+        <div class="text-background">
+          <p>Action, Acventure, Fantasy</p>
+          <h4>Be The First To Know</h4>
+          <div class="icon-down">
+            <i class="fa fa-angle-double-down" aria-hidden="true"></i>
+          </div>
+          <div class="email-input">
+            <input type="text" placeholder="E-mail Address" />
+            <div class="btn-sub"><button>Subscibe</button></div>
+          </div>
+        </div>
+      </div>
+      <div class="headding-events3 row">
+        <div class="headding-left pd15">
+          <i class="fa fa-align-left" aria-hidden="true"></i>
+          <h1>Latest</h1>
+          <h2 class="color">New</h2>
+        </div>
+      </div>
+      <div class="new row pd15">
+        <div class="only-new">
+          <img src="@/assets/image/new1.jpeg" alt="" />
+          <div class="text-new">
+            <h5>Win A Wizarding World Holiday With Fantastic</h5>
+            <div class="icon-new">
+              <i class="fa fa-user" aria-hidden="true"> David Cassidy</i>
+              <i class="fa fa-share" aria-hidden="true"> Cycling</i>
+            </div>
+            <p>
+              Eiusmod tempor incididunt ut labore et dolore magna aliqua.Eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div class="only-new1">
+          <img src="@/assets/image/new2.jpeg" alt="" />
+          <div class="text-new1">
+            <h5>Doctor Strange Assembles With The Avengers</h5>
+            <div class="icon-new">
+              <i class="fa fa-user" aria-hidden="true"> David Cassidy</i>
+              <i class="fa fa-share" aria-hidden="true"> Cycling</i>
+            </div>
+            <p>
+              Eiusmod tempor incididunt ut labore et dolore magna aliqua.Eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div class="only-new2">
+          <img src="@/assets/image/new3.jpeg" alt="" />
+          <div class="text-new1">
+            <h5>Doctor Strange Assembles With The Avengers</h5>
+            <div class="icon-new">
+              <i class="fa fa-user" aria-hidden="true"> David Cassidy</i>
+              <i class="fa fa-share" aria-hidden="true"> Cycling</i>
+            </div>
+            <p>
+              Eiusmod tempor incididunt ut labore et dolore magna aliqua.Eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contents"
+}
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="header">
+      <div class="header-left">
+        <img class="pd15" src="@/assets/image/logo.png" alt="" />
+        <div class="input pd15">
+          <input type="text" name="" required id="" placeholder="Search..." />
+          <div class="search">
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+      <div class="header-right">
+        <ul class="pd15">
+          <li><a href="">Home</a></li>
+          <li class="same">
+            <a href="">Pages</a>
+            <ul class="sub-same">
+              <li><a href="">About Us</a></li>
+              <li><a href="">Pricing</a></li>
+              <li>
+                <a href="">Blog</a>
+                <ul class="child-pages1">
+                  <li><a href="">Blog</a></li>
+                  <li><a href="">Blog Details</a></li>
+                </ul>
+                <i class="fa fa-caret-right" aria-hidden="true"></i>
+              </li>
+              <li>
+                <a href="">Authentication</a>
+                <ul class="child-pages2">
+                  <li><a href="">Login</a></li>
+                  <li><a href="">Register</a></li>
+                </ul>
+                <i class="fa fa-caret-right" aria-hidden="true"></i>
+              </li>
+              <li><a href="">404 Error</a></li>
+            </ul>
+          </li>
+          <li class="same">
+            <a href="">Movies</a>
+            <ul class="sub-same">
+              <li><a href="">Movies List Sidebar</a></li>
+              <li>
+                <a href="">Movies Grid</a>
+                <ul class="child-movies">
+                  <li><a href="">Grid Sidebar</a></li>
+                  <li><a href="">Full Page Grid</a></li>
+                </ul>
+                <i class="fa fa-caret-right" aria-hidden="true"></i>
+              </li>
+              <li><a href="">Movies Details</a></li>
+            </ul>
+          </li>
+          <li class="same">
+            <a href="">Tv Show</a>
+            <ul class="sub-same">
+              <li><a href="">Tv Series</a></li>
+              <li><a href="">Series Details</a></li>
+            </ul>
+          </li>
+          <li class="same">
+            <a href="">Celebrities</a>
+            <ul class="sub-same">
+              <li><a href="">All Celebrities</a></li>
+              <li><a href="">Celebrities Details</a></li>
+            </ul>
+          </li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
